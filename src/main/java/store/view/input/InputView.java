7a1f1dfc6a9fd99dd025768bfbaab27a14778e2e
bowook <mbosx316@naver.com>
2024-11-06 17:50:46 +0900

    feat(InputView) : InputView 기능 추가
    구매할 상품명과 수량 입력 메서드 생성

---
+++ src/main/java/store/view/input/InputView.java
@@ -0,0 +1,15 @@
+package store.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String readItems() {
+        System.out.println(InputMessage.READ_ITEMS.getInputMessage());
+        return readLine();
+    }
+
+    private String readLine() {
+        return Console.readLine();
+    }
+}
