d61e50e4c5e8bbfd5952e21f40a8fd7be0ba0db8
bowook <mbosx316@naver.com>
2024-11-07 18:06:15 +0900

    feat(GeneralProduct) : 프로모션 없는 일반 재고 도메인

--- src/main/java/store/domain/GeneralProduct.java
+++ src/main/java/store/domain/GeneralProduct.java
@@ -1,14 +1,22 @@
 package store.domain;
 
-public class Product {
+public class GeneralProduct {
     private final String name;
-    private final int price;
+    private final String price;
     private String quantity;
 
-    protected Product(final String name, final int price, final String quantity) {
+    public GeneralProduct(final String name, final String price, final String quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
+    @Override
+    public String toString() {
+        return String.format("- %s %,d원 %s개", name, Integer.parseInt(price), quantity);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
