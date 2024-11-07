b7cace8de5cd13ecb902a78980d49e79b9e67dfa
bowook <mbosx316@naver.com>
2024-11-07 22:40:29 +0900

    feat(Storage) : 창고 도메인에서 검증 기능 추가

--- src/main/java/store/domain/Storage.java
+++ src/main/java/store/domain/Storage.java
@@ -1,6 +1,8 @@
 package store.domain;
 
 import java.util.List;
+import store.exception.ConvenienceStoreException;
+import store.exception.ErrorMessage;
 
 public class Storage {
     private final List<GeneralProduct> generalProducts;
@@ -10,6 +12,24 @@ public class Storage {
         this.generalProducts = generalProducts;
         this.promotionProducts = promotionProducts;
     }
+
+    public void validateStorageStatus(List<String> purchaseProduct) {
+        for (String purchaseItem : purchaseProduct) {
+            List<String> item = List.of(purchaseItem.split("-"));
+            validateItemName(item.get(0));
+        }
+    }
+
+    private void validateItemName(String name) {
+        if (!isProductInStorage(name)) {
+            throw ConvenienceStoreException.from(ErrorMessage.NON_EXISTENT_PRODUCT);
+        }
+    }
+
+    private boolean isProductInStorage(String productName) {
+        return generalProducts.stream().anyMatch(product -> product.getName().equals(productName));
+    }
+
 
     public List<GeneralProduct> getGeneralProducts() {
         return generalProducts;
