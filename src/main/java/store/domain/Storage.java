a2e816ead488f881d13dfdb19c1cf3e5175cbe4e
bowook <mbosx316@naver.com>
2024-11-07 18:07:22 +0900

    feat(Storage) : 재고 모음 도메인

--- src/main/java/store/domain/Storage.java
+++ src/main/java/store/domain/Storage.java
@@ -1,2 +1,21 @@
-package store.domain;public class Storage {
+package store.domain;
+
+import java.util.List;
+
+public class Storage {
+    private final List<GeneralProduct> generalProducts;
+    private final List<PromotionProduct> promotionProducts;
+
+    public Storage(final List<GeneralProduct> generalProducts, final List<PromotionProduct> promotionProducts) {
+        this.generalProducts = generalProducts;
+        this.promotionProducts = promotionProducts;
+    }
+
+    public List<GeneralProduct> getGeneralProducts() {
+        return generalProducts;
+    }
+
+    public List<PromotionProduct> getPromotionProducts() {
+        return promotionProducts;
+    }
 }
