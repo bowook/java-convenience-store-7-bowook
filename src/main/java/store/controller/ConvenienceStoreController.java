8c1126d7d9c9a76066c772705031b9c2f4c2ee2a
bowook <mbosx316@naver.com>
2024-11-07 18:04:10 +0900

    feat(ConvenienceStoreController) : 컨트롤러 기능 추가
    환영 메시지 출력
    초기 재고 상태 전달

--- src/main/java/store/controller/ConvenienceStoreController.java
+++ src/main/java/store/controller/ConvenienceStoreController.java
@@ -1,2 +1,26 @@
-package store.controller;public class ConvenienceStroeController {
+package store.controller;
+
+import store.domain.Storage;
+import store.service.StorageService;
+import store.view.input.InputView;
+import store.view.output.OutputView;
+
+public class ConvenienceStoreController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final StorageService storageService;
+
+    public ConvenienceStoreController(InputView inputView, OutputView outputView, StorageService storageService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.storageService = storageService;
+    }
+
+    public void operate() {
+        outputView.writeWelcomeMessage();
+        Storage storage = storageService.initializeStorage();
+        outputView.writeInitStorageStatus(storage);
+    }
+
+
 }
