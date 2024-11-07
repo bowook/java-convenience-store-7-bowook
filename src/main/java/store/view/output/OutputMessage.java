4736fc4b2c6d33c935de14aeea85c46fa2808bf0
bowook <mbosx316@naver.com>
2024-11-07 18:10:14 +0900

    feat(Output) : Output 기능 추가
    - 초기 재고 상태에서 재고 없음 출력
    - 재고 정보 보기 쉽게 관리

--- src/main/java/store/view/output/OutputMessage.java
+++ src/main/java/store/view/output/OutputMessage.java
@@ -1,2 +1,17 @@
-package store.view.output;public class OutputMessage {
+package store.view.output;
+
+public enum OutputMessage {
+    WELCOME_MESSAGE("안녕하세요. W편의점입니다."),
+    SHOW_CURRENT_ITEMS("현재 보유하고 있는 상품입니다."),
+    NEW_LINE(System.lineSeparator());
+
+    private final String outputMessage;
+
+    OutputMessage(String outputMessage) {
+        this.outputMessage = outputMessage;
+    }
+
+    public String getOutputMessage() {
+        return outputMessage;
+    }
 }
--- src/main/java/store/view/output/OutputView.java
+++ src/main/java/store/view/output/OutputView.java
@@ -1,4 +1,72 @@
-package store.view;
+package store.view.output;
+
+import java.util.List;
+import store.domain.GeneralProduct;
+import store.domain.PromotionProduct;
+import store.domain.Storage;
 
 public class OutputView {
+
+    public void writeWelcomeMessage() {
+        System.out.println(OutputMessage.WELCOME_MESSAGE.getOutputMessage());
+    }
+
+    public void writeInitStorageStatus(Storage storage) {
+        System.out.println(OutputMessage.SHOW_CURRENT_ITEMS.getOutputMessage());
+        System.out.print(OutputMessage.NEW_LINE.getOutputMessage());
+        writeInitPromotionProducts(storage);
+        writeInitOnlyGeneralProducts(storage);
+    }
+
+    private void writeInitPromotionProducts(Storage storage) {
+        for (PromotionProduct promotionProduct : storage.getPromotionProducts()) {
+            System.out.println(promotionProduct);
+            boolean flag = false;
+            flag = findEqualGeneralProductName(storage.getGeneralProducts(), promotionProduct.getName(), flag);
+            writeOutOfStock(flag, promotionProduct.getName(), promotionProduct.getPrice());
+        }
+    }
+
+    private boolean findEqualGeneralProductName(List<GeneralProduct> generalProducts, String name, boolean flag) {
+        for (GeneralProduct generalProduct : generalProducts) {
+            if (generalProduct.getName().equals(name)) {
+                System.out.println(generalProduct.toString());
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    private void writeOutOfStock(boolean flag, String name, String price) {
+        if (!flag) {
+            System.out.printf("- %s %,d원 재고 없음", name, Integer.parseInt(price));
+            System.out.print(OutputMessage.NEW_LINE.getOutputMessage());
+        }
+    }
+
+    private void writeInitOnlyGeneralProducts(Storage storage) {
+        for (GeneralProduct generalProduct : storage.getGeneralProducts()) {
+            String generalProductName = generalProduct.getName();
+            boolean flag = false;
+            flag = findEqualPromotionProductName(storage.getPromotionProducts(), generalProductName, flag);
+            writeOnlyGeneralProduct(flag, generalProduct);
+        }
+    }
+
+    private boolean findEqualPromotionProductName(List<PromotionProduct> promotionProducts, String name, boolean flag) {
+        for (PromotionProduct promotionProduct : promotionProducts) {
+            if (promotionProduct.getName().equals(name)) {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    private void writeOnlyGeneralProduct(boolean flag, GeneralProduct generalProduct) {
+        if (!flag) {
+            System.out.println(generalProduct.toString());
+        }
+    }
 }
