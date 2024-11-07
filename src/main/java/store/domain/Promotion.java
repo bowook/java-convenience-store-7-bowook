8031684473e7373a2b8cd62a1423011619fa7de6
bowook <mbosx316@naver.com>
2024-11-07 18:08:46 +0900

    feat(Promotion) : 프로모션 정보 기능 추가

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -2,16 +2,22 @@ package store.domain;
 
 import java.time.LocalDate;
 
-public class Promotions {
+public class Promotion {
     private final String name;
     private final int buy;
     private final int get;
     private final LocalDate startDate;
     private final LocalDate endDate;
 
-    public Promotions() {
-        this.onePlusOne = false;
-        this.twoPlusOne = false;
-        this.flashSale = false;
+    public Promotion(String name, int buy, int get, String startDate, String endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = LocalDate.parse(startDate);
+        this.endDate = LocalDate.parse(endDate);
+    }
+
+    public String getName() {
+        return name;
     }
 }
