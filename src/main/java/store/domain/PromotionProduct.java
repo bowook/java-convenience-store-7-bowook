1f5c9894d0e729aae47ddbea57533d79a40238a0
bowook <mbosx316@naver.com>
2024-11-07 18:06:41 +0900

    feat(PromotionProduct) : 프로모션 있는 재고 도메인

--- src/main/java/store/domain/PromotionProduct.java
+++ src/main/java/store/domain/PromotionProduct.java
@@ -1,2 +1,28 @@
-package store.domain;public class PromotionProduct {
+package store.domain;
+
+public class PromotionProduct {
+    private final String name;
+    private final String price;
+    private String quantity;
+    private String promotion;
+
+    public PromotionProduct(final String name, final String price, String quantity, String promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("- %s %,d원 %s개 %s", name, Integer.parseInt(price), quantity, promotion);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
 }
