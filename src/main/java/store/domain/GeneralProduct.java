0805213574a19dcafff46efaa57ef442517b5fa8
bowook <mbosx316@naver.com>
2024-11-09 19:55:20 +0900

    feat(GeneralProduct) : 상품 수량 차감 기능 추가

--- src/main/java/store/domain/GeneralProduct.java
+++ src/main/java/store/domain/GeneralProduct.java
@@ -3,9 +3,9 @@ package store.domain;
 public class GeneralProduct {
     private final String name;
     private final String price;
-    private String quantity;
+    private int quantity;
 
-    public GeneralProduct(final String name, final String price, final String quantity) {
+    public GeneralProduct(final String name, final String price, final int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
@@ -13,7 +13,7 @@ public class GeneralProduct {
 
     @Override
     public String toString() {
-        if ("0".equals(quantity)) {
+        if (quantity == 0) {
             return String.format("- %s %,d원 재고 없음", name, Integer.parseInt(price));
         }
         return String.format("- %s %,d원 %s개", name, Integer.parseInt(price), quantity);
@@ -24,6 +24,10 @@ public class GeneralProduct {
     }
 
     public int getQuantity() {
-        return Integer.parseInt(quantity);
+        return quantity;
+    }
+
+    public void subtraction(int value) {
+        this.quantity -= Math.abs(value);
     }
 }
