5b1e26aa3a5b3d0c3cfa14acfc7e410c94126fb0
bowook <mbosx316@naver.com>
2024-11-09 19:53:49 +0900

    feat(Compare) : 비교 책임 기능

--- src/main/java/store/utils/Compare.java
+++ src/main/java/store/utils/Compare.java
@@ -1,2 +1,15 @@
-package store.utils;public class Compare {
+package store.utils;
+
+import store.domain.PromotionProduct;
+
+public class Compare {
+    public static boolean checkFreeOneMore(int remainingPurchase, int remainPromotionStock,
+                                           PromotionProduct promotionProduct) {
+        return (remainingPurchase == promotionProduct.getPromotionGetBuy() && remainPromotionStock >= 1);
+
+    }
+
+    public static boolean checkSupplementStock(int remainStock) {
+        return remainStock < 0;
+    }
 }
