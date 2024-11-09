f2f40f7f7b2390b9285b7e0e8fc084879eb45ba3
bowook <mbosx316@naver.com>
2024-11-10 02:38:59 +0900

    feat(ReceiptItem) : 영수증 도메인 추가

---
+++ src/main/java/store/domain/ReceiptItem.java
@@ -0,0 +1,35 @@
+package store.domain;
+
+public class ReceiptItem {
+    private final String itemName;
+    private final int buyQuantity;
+    private final int getQuantity;
+    private final int price;
+
+    public ReceiptItem(final String itemName, final int buyQuantity, final int getQuantity, final int price) {
+        this.itemName = itemName;
+        this.buyQuantity = buyQuantity;
+        this.getQuantity = getQuantity;
+        this.price = price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getBuyQuantity() {
+        return buyQuantity;
+    }
+
+    public int getTotalBuyQuantity() {
+        return buyQuantity + getQuantity;
+    }
+
+    public int getGetQuantity() {
+        return getQuantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
