dd2551900694b02491bbb91e5d03c0527820c98f
bowook <mbosx316@naver.com>
2024-11-10 19:50:21 +0900

    feat(CommonMessage) : Yes Or No 상수

--- src/main/java/store/constant/CommonMessage.java
+++ src/main/java/store/constant/CommonMessage.java
@@ -1,2 +1,17 @@
-package store.constant;public enum CommonMessage {
+package store.constant;
+
+public enum CommonMessage {
+    YES("Y"),
+    NO("N");
+
+
+    private final String commonMessage;
+
+    CommonMessage(final String commonMessage) {
+        this.commonMessage = commonMessage;
+    }
+
+    public String getCommonMessage() {
+        return commonMessage;
+    }
 }
