0fa1f60e7c4010f84567ae86046c08b5eaa56adc
bowook <mbosx316@naver.com>
2024-11-10 19:51:06 +0900

    feat(SignMessage) : 기호 상수 처리

--- src/main/java/store/constant/SignMessage.java
+++ src/main/java/store/constant/SignMessage.java
@@ -1,2 +1,18 @@
-package store.constant;public class SignMessage {
+package store.constant;
+
+public enum SignMessage {
+    COMMA(","),
+    LEFT_SQUARE_BRACKET("["),
+    RIGHT_SQUARE_BRACKET("]"),
+    HYPHEN("-");
+
+    private final String sign;
+
+    SignMessage(final String sign) {
+        this.sign = sign;
+    }
+
+    public String getSign() {
+        return sign;
+    }
 }
