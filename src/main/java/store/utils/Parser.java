265891c06acc28a668668713a61d170f6f73dadd
bowook <mbosx316@naver.com>
2024-11-10 19:51:31 +0900

    feat(Parser) : 정수로 파싱하는 기능 추가

--- src/main/java/store/utils/Parser.java
+++ src/main/java/store/utils/Parser.java
@@ -1,2 +1,7 @@
-package store.utils;public class Parser {
+package store.utils;
+
+public class Parser {
+    public static int parse(String number) {
+        return Integer.parseInt(number);
+    }
 }
